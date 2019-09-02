f5c4cdd50d9325597075696f4baf7a075afe4075
wasimafser <wasim.afser@gmail.com>
2019-09-02 18:21:23 +0530

    pin message : fix logger error ( should read docs lol) heroku: add run def

--- bot.py
+++ bot.py
@@ -46,7 +46,8 @@ def random_handler(update, context):
 
 
 def pin_handler(update, context):
-    logger.info(update.effective_message['message_id'], type(update.effective_message['message_id']))
+    logger.info(update.effective_message['message_id'])
+    logger.info(type(update.effective_message['message_id']))
     Bot.pin_chat_message(update.effective_chat['id'], update.effective_message['message_id'], disable_notification=True)
 
 
